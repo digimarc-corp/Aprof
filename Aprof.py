99b5e8e08bfdec96f210154e05ae9c0e54cabc65
Scott Long <slong@digimarc.com>
2014-10-22 13:41:44 -0700

    Add '-s devid' switch to Aprof, same meaning as for adb (set device serial)
    Copied from Perforce
     Change: 174673

--- Aprof.py
+++ Aprof.py
@@ -3,6 +3,13 @@ import glob
 
 from Aprof.Profiler import *
 
+# Look for '-s devid' flag
+for i in xrange(1, len(sys.argv) - 1):
+	if sys.argv[i] == '-s':
+		# Set ANDROID_SERIAL to force adb to use the indicated device
+		os.environ['ANDROID_SERIAL'] = sys.argv[i + 1]
+		break
+
 localfiles = []
 for i in xrange(1, len(sys.argv) - 1):
 	localfiles += glob.glob(sys.argv[i])
