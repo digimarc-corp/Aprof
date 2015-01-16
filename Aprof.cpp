61a2ea08e82359d89fb85f979184b08d4ca88a9c
Chris Haverkate <chris.haverkate@digimarc.com>
2015-01-16 15:10:20 -0800

    Fixed it

--- Aprof.cpp
+++ Aprof.cpp
@@ -7,17 +7,8 @@
 #include <pthread.h>
 #include "Aprof.h"
 
-/*  Android NDK doesn't have its own ucontext.h */
 #include <asm/sigcontext.h>       /* for sigcontext */
 #include <asm/signal.h>           /* for stack_t */
-
-typedef struct ucontext {
-        unsigned long uc_flags;
-        struct ucontext *uc_link;
-        stack_t uc_stack;
-        struct sigcontext uc_mcontext;
-        unsigned long uc_sigmask;
-} ucontext_t;
 
 namespace GUM {
 
